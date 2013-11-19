3fdf89ee534c3b217362e8019320916517cfe1dc
justinxaustin <justin.austin@bison.howard.edu>
2013-11-19 13:09:12 -0500

    Justin Austin bsearch

--- projects/bsearch/BlackExcellencebsearch.py
+++ projects/bsearch/BlackExcellencebsearch.py
@@ -1,1 +1,21 @@
 Bsearch
+
+# Justin Austin
+
+def binary_search(a, x, small=0, big=None):
+    if big is None:
+        big = len(a)
+    while small < big:
+        mid = (small+big)//2
+        midval = a[mid]
+        if midval < x:
+            small = mid+1
+        elif midval > x: 
+            big = mid
+        else:
+            return mid
+    return -1
+    
+    
+    
+    
